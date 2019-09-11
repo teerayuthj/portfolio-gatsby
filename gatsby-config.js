4f7759738a15b17bac08fdafb2a56ad56c178bc3
Teerayuth Thanakornyingyong <teerayuth.james@gmail.com>
2019-09-11 14:34:17 +0700

    delete gatsby-transformer-sharp & plugin-sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `๋James | Teerayuth`,
+    title: `James | Teerayuth`,
     description: `FontEnd Developrt - React`,
     author: `James Teerayuht`,
   },
@@ -24,8 +24,7 @@ module.exports = {
         path: `${__dirname}/src/data`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -34,7 +33,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/logo/fire-solid.svg`, // This path is relative to the root of the site.
       },
     },
