442b727849ffacd021c12441486e3e3e1b86daa4
James Teerayuth <teerayuth.james@gmail.com>
2019-10-15 13:23:06 +0700

    Change fade heading

--- src/sections/Heading.js
+++ src/sections/Heading.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react"
 import { Heading, Flex, Box, Text } from "rebass"
 import TextLoop from "react-text-loop"
-import Fade from "react-reveal/Fade"
 import Section from "../components/Section"
 import SocialLink from "../components/Social"
 
@@ -26,49 +25,47 @@ const links = [
 const HeadingPage = () => (
   <Section.Container id="home">
     <Fragment>
-      <Fade bottom>
-        <Heading
-          textAlign="center"
-          as="h1"
-          color="primary"
-          fontSize={[5, 6, 7]}
-          mb={[3, 4, 5]}
-        >
-          {`Hello, I'm Teerayuth Thanakornyingyong!`}
-        </Heading>
-        <Heading
-          textAlign="center"
-          as="h3"
-          color="primary"
-          fontSize={[4, 5, 6]}
-          mb={[1, 2, 3]}
-        >
-          {`Fontend Developer`}
-        </Heading>
-        <Heading
-          as="h2"
-          color="primary"
-          fontSize={[4, 5, 6]}
-          mb={[3, 5]}
-          textAlign="center"
-        >
-          <TextLoop>
-            {["React", "JavaScript", " CSS", "GraphQL"].map(text => (
-              <Text width={[300, 500]} key={text}>
-                {text}
-              </Text>
-            ))}
-          </TextLoop>
-        </Heading>
-
-        <Flex alignItems="center" justifyContent="center" flexWrap="wrap">
-          {links.map(link => (
-            <Box mx={3} fontSize={[5, 6, 6]} key={link.name}>
-              <SocialLink {...link} />
-            </Box>
+      <Heading
+        textAlign="center"
+        as="h1"
+        color="primary"
+        fontSize={[5, 6, 7]}
+        mb={[3, 4, 5]}
+      >
+        {`Hello, I'm Teerayuth Thanakornyingyong!`}
+      </Heading>
+      <Heading
+        textAlign="center"
+        as="h3"
+        color="primary"
+        fontSize={[4, 5, 6]}
+        mb={[1, 2, 3]}
+      >
+        {`Fontend Developer`}
+      </Heading>
+      <Heading
+        as="h2"
+        color="primary"
+        fontSize={[4, 5, 6]}
+        mb={[3, 5]}
+        textAlign="center"
+      >
+        <TextLoop>
+          {["React", "JavaScript", " CSS", "GraphQL"].map(text => (
+            <Text width={[300, 500]} key={text}>
+              {text}
+            </Text>
           ))}
-        </Flex>
-      </Fade>
+        </TextLoop>
+      </Heading>
+
+      <Flex alignItems="center" justifyContent="center" flexWrap="wrap">
+        {links.map(link => (
+          <Box mx={3} fontSize={[5, 6, 6]} key={link.name}>
+            <SocialLink {...link} />
+          </Box>
+        ))}
+      </Flex>
     </Fragment>
   </Section.Container>
 )
